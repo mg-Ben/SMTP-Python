6a302e1ccd4ce198255d3f3934ba72eb6220927b
bmartin <benjaminmartin1008@gmail.com>
2023-12-22 12:55:09 +0100

    gmail changes commit

--- client/client.py
+++ client/client.py
@@ -1,5 +1,5 @@
 from smtplib import SMTP as Client
 
 client = Client(host="0.0.0.0", port=8088)
-client.sendmail("origin@example.com", ["destination@example.com"],
+client.sendmail("origin@gmail.com", ["benjaminmartin1008@gmail.com"],
                 "Hello world :D\n")
